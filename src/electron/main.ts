e165f7525924c42b6256c5e887d18ae7c8e18095
Fantom <93797224+Fantom-01@users.noreply.github.com>
2024-10-13 19:08:16 +0100

    making ipc connections wrapped in type safety

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,24 +1,26 @@
-import { app, BrowserWindow, ipcMain } from "electron"
-import path from "path"
-import { isDev } from "./util.js"
-import { getStaticData, pollResources } from "./resourceManager.js"
-import { getPreloadPath } from "./pathResolver.js"
+import { app, BrowserWindow, ipcMain } from "electron";
+import path from "path";
+import { ipcMainHandle, isDev } from "./util.js";
+import { getStaticData, pollResources } from "./resourceManager.js";
+import { getPreloadPath } from "./pathResolver.js";
 
 app.on("ready", () => {
-    const mainWindow = new BrowserWindow({
-        webPreferences: {
-            preload: getPreloadPath(),
-        }
-    })
-    if (isDev()) {
-        mainWindow.loadURL("http://localhost:5123")
-    } else {
-        mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"))
-    }
+	const mainWindow = new BrowserWindow({
+		webPreferences: {
+			preload: getPreloadPath(),
+		},
+	});
+	if (isDev()) {
+		mainWindow.loadURL("http://localhost:5123");
+	} else {
+		mainWindow.loadFile(
+			path.join(app.getAppPath(), "/dist-react/index.html")
+		);
+	}
 
-    pollResources(mainWindow);
+	pollResources(mainWindow);
 
-    ipcMain.handle("getStaticData", () => {
-        return getStaticData()
-    })
-})
+	ipcMainHandle("getStaticData", () => {
+		return getStaticData();
+	});
+});
