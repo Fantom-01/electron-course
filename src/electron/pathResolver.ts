d39d4d8baa7f205ef26c61bc4505d4b30ffc7c08
Fantom <93797224+Fantom-01@users.noreply.github.com>
2024-10-13 17:44:29 +0100

    pre type-safety, trnsf data from electron to browserwindow

---
+++ src/electron/pathResolver.ts
@@ -0,0 +1,11 @@
+import path, { join } from "path"
+import { app } from "electron"
+import { isDev } from "./util.js"
+
+export function getPreloadPath() {
+    return path.join(
+        app.getAppPath(),
+        isDev() ? '.' : '..',
+        '/dist-electron/preload.cjs'
+    )
+}
