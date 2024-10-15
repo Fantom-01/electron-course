497127af7490d4cf19f46d6b9311abc3d3e67ee6
Fantom <93797224+Fantom-01@users.noreply.github.com>
2024-10-15 13:42:45 +0100

    event validation

--- src/electron/pathResolver.ts
+++ src/electron/pathResolver.ts
@@ -1,11 +1,15 @@
-import path, { join } from "path"
-import { app } from "electron"
-import { isDev } from "./util.js"
+import path, { join } from "path";
+import { app } from "electron";
+import { isDev } from "./util.js";
 
 export function getPreloadPath() {
-    return path.join(
-        app.getAppPath(),
-        isDev() ? '.' : '..',
-        '/dist-electron/preload.cjs'
-    )
+	return path.join(
+		app.getAppPath(),
+		isDev() ? "." : "..",
+		"/dist-electron/preload.cjs"
+	);
+}
+
+export function getUIPath() {
+	return path.join(app.getAppPath(), "/dist-react/index.html");
 }
